c5bd8eae47a9fc386c78f440a93b8ef6b5a9c4c1
Einar Forselv <eforselv@gmail.com>
2020-02-05 20:16:25 +0100

    Separate game map from window

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import moderngl
 import moderngl_window
 from pyrr import Matrix44
@@ -5,9 +7,35 @@ from moderngl_window.scene.camera import KeyboardCamera
 from track import BSTrack
 
 
+class BeatSaberMap:
+
+    def __init__(self, scene, track):
+        self.scene = scene
+        self.track = track
+
+        # Objects & Materials
+        self.mat_back_lights = self.scene.find_material(name="Back Lights")
+
+        # Static unlit par of the map
+        self.highway = self.scene.find_node(name="Highway")
+
+        # Rings
+        self.ring_01 = self.scene.find_node(name='Ring.001')
+        self.ring_02 = self.scene.find_node(name='Ring.001')
+
+        # Light rails on left and right side
+        self.right_static = self.scene.find_node(name="Right Static")
+        self.left_static = self.scene.find_node(name="Left Static")
+
+    def render(self, camera, time, frame_time):
+        self.mat_back_lights.color = math.fmod(time, 1.0), 0.0, 0.0, 1.0
+        self.scene.draw(camera.projection.matrix, camera.matrix)
+
+
 class BeatSaber(moderngl_window.WindowConfig):
     title = "Beat Saber Light Show"
     resource_dir = './resources'
+    window_size = 1920, 1080
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -16,17 +44,16 @@ class BeatSaber(moderngl_window.WindowConfig):
         self.camera = KeyboardCamera(self.wnd.keys, near=1.0, far=1000.0)
         self.camera.velocity = 50
         self.camera_enabled = True
-        self.scene = self.load_scene('bs_map.glb')
-        self.track = BSTrack('./resources/Lightshow.dat')
+
+        self.map = BeatSaberMap(
+            self.load_scene('bs_map.glb'),
+            BSTrack('./resources/Lightshow.dat'),
+        )
 
     def render(self, time, frame_time):
         self.ctx.enable(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
         self.wnd.clear(0.25, 0.25, 0.25)
-
-        self.scene.draw(
-            self.camera.projection.matrix,
-            self.camera.matrix,
-        )
+        self.map.render(self.camera, time, frame_time)
 
     def key_event(self, key, action, modifiers):
         keys = self.wnd.keys
