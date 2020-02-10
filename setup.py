2b54f8a91c973ce29e57f6638f430523ce5b1ec6
Einar Forselv <eforselv@gmail.com>
2020-02-10 23:21:35 +0100

    Create setup.py
    We want this to be a proper package

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name="beatsaber",
+    version="0.1.1",
+    description="Beat Saber player",
+    url="https://github.com/einarf/beatsaber",
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Einar Forselv",
+    author_email="eforselv@gmail.com",
+    packages=find_namespace_packages(include=['beatsaber', 'beatsaber.*']),
+    python_requires='>=3.5',
+    keywords=['moderngl', 'moderngl-window', 'beat saber'],
+    platforms=['any'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Games/Entertainment',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Multimedia :: Graphics :: 3D Rendering',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    # requires=[
+    #     'pyglet>=1.5,<2',
+    #     'moderngl_window>=2.1,<3',
+    # ],
+    entry_points={'console_scripts': [
+        'beatsaber = beatsaber.main:run_from_cmd',
+    ]},
+)
